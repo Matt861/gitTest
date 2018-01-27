6c16f5c18e110e780457bff752217c3d0a744f05
Matt861 <mattwindham21@gmail.com>
2018-01-26 18:59:33 -0600

    added HelloWorld.cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,19 @@
+//
+//  HelloWorld.cpp
+//  MattGit
+//
+//  Created by Matt Windham on 1/26/18.
+//  Copyright © 2018 Matt Windham. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    cout << "Hello World" << endl;
+    cout << endl;
+    
+    return 0;
+}
